dd0c9cdee34e9baaf6039629cff8a3b343cca560
mohamedsheikh999 <mohamedsheikh1@student.moringaschool.com>
2025-05-27 11:00:45 +0300

    i commit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,25 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
-        
+    def __init__(self, title, author, pages=1):
+        self.title = title
+        self.author = author
+        self.pages = pages  # uses setter
+
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+            return
+        if value <= 0:
+            print("page_count must be positive")
+            return
+        self._pages = value
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    def __str__(self):
+        return f"'{self.title}' by {self.author}, {self.pages} pages"
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,28 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size, color=None):
+        self.brand = brand
+        self.color = color
+        self.condition = "Used"
+        self.size = size  # use setter
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+            return
+        if value <= 0:
+            print("size must be a positive number")
+            return
+        self._size = value
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+    def __str__(self):
+        color_str = self.color if self.color else "Unknown color"
+        return f"{color_str} {self.brand} shoe, size {self.size}"
